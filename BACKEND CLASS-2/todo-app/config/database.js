761b757061198976848606da1ead7fd1ea6a3e13
AMAN SINGH <amansinghbhumihar2510@gmail.com>
2025-04-01 17:48:02 +0530

    Added congif

---
+++ BACKEND CLASS-2/todo-app/config/database.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const dbConnect = () => {
+    mongoose.connect(process.env.DATABSE_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+        .then(() => console.log("Database connected successfully"))
+        .catch((err) => {
+            console.error("Database connection failed:");
+            console.log(err.message);
+            process.exit(1);
+        });
+
+}
+
+module.exports = dbConnect;
